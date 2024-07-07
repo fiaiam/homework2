83048fb8f29cda2e092ac0407c29f2ac8a09e0c7
fiaiam <fiaiam@yandex.ru>
2024-07-07 16:37:47 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_hw_done = 12
+hours_used = 1.5
+course_name = 'Python'
+time_for_1_task = (hours_used / number_of_hw_done)
+print('Курс:', course_name,', всего задач:', number_of_hw_done,', затрачено часов:', hours_used,', среднее время выполнения:',time_for_1_task, 'часа')
